ef865abdb9f83ab41939b40caf36dbf366b2e14d
Diwakar0793 <54579283+Diwakar0793@users.noreply.github.com>
2025-03-04 11:20:18 +0530

    first commit

---
+++ src/app/components/pages/apple-business/apple-business.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-apple-business',
+  standalone: true,
+  imports: [],
+  templateUrl: './apple-business.component.html',
+  styleUrl: './apple-business.component.scss'
+})
+export class AppleBusinessComponent {
+
+}
